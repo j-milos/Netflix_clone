0546ec6e62a8f1e0ff9cb49716058a4f077ac1af
Milos Jovanovic <jovanovic.milos977@gmail.com>
2024-07-27 20:13:21 +0200

    sign_in_page_design

---
+++ app/auth/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useState } from "react";
+import Input from "@/components/Input";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpeg')] bg-no-repeat bg-center bg-cover">
+      <div className="bg-black w-full h-full bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="Logo" className="h-12" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black/70 px-8 py-8 self-center rounded-md">
+            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <div className="flex flex-col gap-4">
+              <Input
+                label="Username"
+                onChange={(ev: any) => setName(ev.target.value)}
+                id="name"
+                value={name}
+              />
+              <Input
+                label="Email"
+                onChange={(ev: any) => setEmail(ev.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Input
+                label="Password"
+                onChange={(ev: any) => setPassword(ev.target.value)}
+                id="password"
+                type="password"
+                value={password}
+              />
+            </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-12 hover:bg-red-700 transition">
+              Login
+            </button>
+            <p className="text-neutral-500 mt-4">
+              First time using Netflix?
+              <span className="text-white ml-1 hover:underline cursor:pointer">
+                Create an account
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
